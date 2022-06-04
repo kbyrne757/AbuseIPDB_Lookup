a94d9d0744bd0c003ecaaa29fb05391e60c9d848
Lemon Zest <k.byrne357@gmail.com>
2022-06-04 19:58:50 +0100

    Requests Import & apikey

--- AbuseIPDB_Lookup.py
+++ AbuseIPDB_Lookup.py
@@ -1,6 +1,17 @@
 #Python V 3.10.0
+try:
+    import requests
+except ModuleNotFoundError:
+    package_name = "requests"
+    print(package_name + " is not installed")
+    print("Attempting install of " + package_name)
+    sp.check_call([sys.executable, "-m", "pip", "install", package_name])
+    sleep(0.5)
+    import requests
+    print("Import Succesful")
 
 
+apikey = input("Enter your AbuseIPDB API Key: ")
 
 
 
