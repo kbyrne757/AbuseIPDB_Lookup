6d0a9c1102b7899311430954239179b6ac493c20
Lemon Zest <k.byrne357@gmail.com>
2022-06-04 22:06:31 +0100

    Functional Menu & key writing

--- AbuseIPDB_Lookup.py
+++ AbuseIPDB_Lookup.py
@@ -1,5 +1,7 @@
 #Python V 3.10.0
 import json
+import os
+import getpass
 try:
     import requests
 except ModuleNotFoundError:
@@ -11,8 +13,33 @@ except ModuleNotFoundError:
     import requests
     print("Import Succesful")
 
+try:
+    from art import *
+except ModuleNotFoundError:
+    package_name = "art"
+    print(package_name + " is not installed")
+    print("Attempting install of " + package_name)
+    sp.check_call([sys.executable, "-m", "pip", "install", package_name])
+    sleep(0.5)
+    from art import *
+    print("Import Succesful")
 
-apikey = input("Enter your AbuseIPDB API Key: ")
+#function to acquire username for directory listing
+def getUser():
+    username = getpass.getuser()
+    return username
+
+
+def main():
+    tprint("IP Lookup",font="block",chr_ignore=True)
+    MainMenu()
+apikeyfile = ("C:\\Users\\" + getUser() + "\\Documents\\abuseipdblookupapikey.txt")
+def ApiKey():
+    apikey = input("Enter your AbuseIPDB API Key: ")
+    
+    with open(apikeyfile, 'w') as f:
+        f.writelines(apikey)
+    print("API KEY written to " + apikeyfile)
 
 
 
@@ -22,19 +49,24 @@ def AbuseLookup():
 
     IPAddr = input("Enter IP Address to check: ")
 
+    with open(apikeyfile) as f:
+        APIKEY = f.read()
+
     querystring = {
     'ipAddress' : IPAddr,
     }
 
     headers = {
     'Accept': 'application/json',
-    'key' : apikey
+    'key' : APIKEY
     }
 
     response = requests.request(method='GET', url=url, headers=headers, params=querystring)
 
     decodedResponse = json.loads(response.text)
     print (json.dumps(decodedResponse, sort_keys=True, indent=4))
+
+    
 
 
 def MainMenu():
@@ -50,12 +82,14 @@ def MainMenu():
 def userOptions(options):
 
     if (options == "1"):
-        print(test)
+        ApeKey()
     elif (options == "2"):
-        print(test)
+        AbuseLookup()
     elif (options == "3"):
         print(test)
     elif (options == "4"):
         print(test)
     elif (options == "0"):
         print(test) 
+
+main()
