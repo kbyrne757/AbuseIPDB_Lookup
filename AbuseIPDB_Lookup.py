42edb4cd9b7772c2a08bca79ec2124344f981e16
Lemon Zest <k.byrne357@gmail.com>
2022-06-21 16:38:29 +0100

    added option for multiple comments for Reporting IP's

--- AbuseIPDB_Lookup.py
+++ AbuseIPDB_Lookup.py
@@ -40,7 +40,6 @@ def getUser():
 
 #Setting api key file location
 apikeyfile = ("C:\\Users\\" + getUser() + "\\Documents\\abuseipdblookupapikey.txt")
-    
 
 
 
@@ -116,40 +115,41 @@ def MainMenu():
     print("OPTION 0: Quit the program\n")
     userOptions(input())
 
-def ReportCategories():
-    print("OPTION 1: DNS Compromise      Altering DNS records resulting in improper redirection \n")
-    print("OPTION 2: DNS Poisoining      Falsifying domain server cache (cache poisoning) \n")
-    print("OPTION 3: Fraud Orders        Fraudulent orders \n")
-    print("OPTION 4: DDoS Attack         Participating in distributed denial-of-service (usually part of botnet) \n")
-    print("OPTION 5: FTP Brute-Force \n")
-    print("OPTION 6: Ping of Death       Oversized IP packet \n")
-    print("OPTION 7: Phishing            Phishing websites and/or email  \n")
-    print("OPTION 8: Fraud VoIP \n")
-    print("OPTION 9: Open Proxy          Open proxy, open relay, or Tor exit node \n")
-    print("OPTION 10: Web Spam           Comment/forum spam, HTTP referer spam, or other CMS spam \n")
-    print("OPTION 11: Email Spam         Spam email content, infected attachments, and phishing emails. Note: Limit comments to only relevent information (instead of log dumps) and be sure to remove PII if you want to remain anonymous.  \n")
-    print("OPTION 12: Blog Spam          CMS blog comment spam  \n")
-    print("OPTION 13: VPN IP             Conjunctive category   \n")
-    print("OPTION 14: Port Scan          Scanning for open ports and vulnerable services   \n")
-    print("OPTION 15: Hacking \n")
-    print("OPTION 16: SQL Injection      Attempts at SQL injection   \n")
-    print("OPTION 17: Spoofing           Email sender spoofing    \n")
-    print("OPTION 18: Brute-Force        Credential brute-force attacks on webpage logins and services like SSH, FTP, SIP, SMTP, RDP, etc. This category is seperate from DDoS attacks.     \n")
-    print("OPTION 19: Bad Web Bot        Webpage scraping (for email addresses, content, etc) and crawlers that do not honor robots.txt. Excessive requests and user agent spoofing can also be reported here.    \n")
-    print("OPTION 20: Exploited Host     Host is likely infected with malware and being used for other attacks or to host malicious content. The host owner may not be aware of the compromise. This category is often used in combination with other attack categories.     \n")
-    print("OPTION 21: Web App Attack     Attempts to probe for or exploit installed web applications such as a CMS like WordPress/Drupal, e-commerce solutions, forum software, phpMyAdmin and various other software plugins/solutions.     \n")
-    print("OPTION 22: SSH                Secure Shell (SSH) abuse. Use this category in combination with more specific categories    \n")
-    print("OPTION 23: IoT Targeted        Abuse was targeted at an 'Internet of Things' type device. Include information about what type of device was targeted in the comments    \n")
-    print("OPTION 0: Main Menu \n")
-    ReportIP(input())
-
-def ReportIP(option):
-    category = 0
-    category = option
-    print(category)
-    if (category == '0'):
-        MainMenu()
-        
+def ReportIP():
+    category = []
+    looptimes = int(input("How many categories would you like to add? \n"))
+    for x in range(looptimes):
+        print("OPTION 1: DNS Compromise      Altering DNS records resulting in improper redirection \n")
+        print("OPTION 2: DNS Poisoining      Falsifying domain server cache (cache poisoning) \n")
+        print("OPTION 3: Fraud Orders        Fraudulent orders \n")
+        print("OPTION 4: DDoS Attack         Participating in distributed denial-of-service (usually part of botnet) \n")
+        print("OPTION 5: FTP Brute-Force \n")
+        print("OPTION 6: Ping of Death       Oversized IP packet \n")
+        print("OPTION 7: Phishing            Phishing websites and/or email  \n")
+        print("OPTION 8: Fraud VoIP \n")
+        print("OPTION 9: Open Proxy          Open proxy, open relay, or Tor exit node \n")
+        print("OPTION 10: Web Spam           Comment/forum spam, HTTP referer spam, or other CMS spam \n")
+        print("OPTION 11: Email Spam         Spam email content, infected attachments, and phishing emails. Note: Limit comments to only relevent information (instead of log dumps) and be sure to remove PII if you want to remain anonymous.  \n")
+        print("OPTION 12: Blog Spam          CMS blog comment spam  \n")
+        print("OPTION 13: VPN IP             Conjunctive category   \n")
+        print("OPTION 14: Port Scan          Scanning for open ports and vulnerable services   \n")
+        print("OPTION 15: Hacking \n")
+        print("OPTION 16: SQL Injection      Attempts at SQL injection   \n")
+        print("OPTION 17: Spoofing           Email sender spoofing    \n")
+        print("OPTION 18: Brute-Force        Credential brute-force attacks on webpage logins and services like SSH, FTP, SIP, SMTP, RDP, etc. This category is seperate from DDoS attacks.     \n")
+        print("OPTION 19: Bad Web Bot        Webpage scraping (for email addresses, content, etc) and crawlers that do not honor robots.txt. Excessive requests and user agent spoofing can also be reported here.    \n")
+        print("OPTION 20: Exploited Host     Host is likely infected with malware and being used for other attacks or to host malicious content. The host owner may not be aware of the compromise. This category is often used in combination with other attack categories.     \n")
+        print("OPTION 21: Web App Attack     Attempts to probe for or exploit installed web applications such as a CMS like WordPress/Drupal, e-commerce solutions, forum software, phpMyAdmin and various other software plugins/solutions.     \n")
+        print("OPTION 22: SSH                Secure Shell (SSH) abuse. Use this category in combination with more specific categories    \n")
+        print("OPTION 23: IoT Targeted        Abuse was targeted at an 'Internet of Things' type device. Include information about what type of device was targeted in the comments    \n")
+        print("OPTION 0: Main Menu \n")
+        option = int(input("category num: \n"))
+        if option == 0:
+            MainMenu()
+        elif option not in category:
+            category.append(option)
+        else: print("Error Category already present!")
+    values = ', '.join(str(v) for v in category)
     url = "https://api.abuseipdb.com/api/v2/report"
 
     with open(apikeyfile) as f:
@@ -163,7 +163,7 @@ def ReportIP(option):
         ReportIpComment = input("Enter Comment: ")
         params = {
             'ip': IPAddr,
-            'categories': category,
+            'categories': values,
             'comment': ReportIpComment
                 }
 
@@ -182,7 +182,7 @@ def ReportIP(option):
         print("No comment added, proceeding......")
         params = {
             'ip': IPAddr,
-            'categories': category,
+            'categories': values,
                 }
 
         headers = {
@@ -228,7 +228,7 @@ def userOptions(options):
     elif (options == "2"):
         AbuseLookup()
     elif (options == "3"):
-        ReportCategories()
+        ReportIP()
     elif (options == "4"):
         ClearAddress()
     elif (options == "5"):
